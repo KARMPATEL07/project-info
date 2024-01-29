1a93fcaa23d9e785d5ce77bb582b452fa5216dc2
KARMPATEL07 <karmpatel432.pf@gmail.com>
2024-01-29 22:41:53 +0530

    inital commit

---
+++ frontend/src/pages/homepage/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from "../../component/navbar/Navbar";
+import Footer from '../../component/footer/Footer'
+
+const Home = () => {
+  return (
+    <>
+    <Navbar />
+    <div>Home</div>
+    <Footer />
+    </>
+    
+  )
+}
+
+export default Home
